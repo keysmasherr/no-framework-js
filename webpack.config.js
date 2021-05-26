2a784481005313447e127580d4c56e8e382eeb00
keysmasherr <keysmasherr@gmail.com>
2021-05-26 20:26:51 +0530

    build package changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
 
@@ -38,12 +39,45 @@ module.exports = {
             filename: '[name].css',
             chunkFilename: '[id].css'
         }),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                // Lossless optimization with custom option
+                // Feel free to experiment with options for better result for you
+                plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 5 }]
+                ],
+            },
+        })
     ],
     module: {
         rules: [{
             test: /\.js$/,
             use: {
                 loader: 'babel-loader'
+            }
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: ''
+                }
+            }, {
+                loader: 'css-loader'
+            }, {
+                loader: 'postcss-loader'
+            }, {
+                loader: 'sass-loader'
+            }]
+        }, {
+            test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+            loader: 'file-loader',
+            options: {
+                // name(file) {
+                //     return '[hash].[ext]'
+                // }
             }
         }]
     }
