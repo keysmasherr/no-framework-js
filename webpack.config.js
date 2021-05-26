4c86042e39001c62b38f284c8ceb7e37fa53073b
keysmasherr <keysmasherr@gmail.com>
2021-05-26 21:16:54 +0530

    webpack build file

--- webpack.config.js
+++ webpack.config.js
@@ -79,6 +79,26 @@ module.exports = {
                 //     return '[hash].[ext]'
                 // }
             }
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: [{
+                loader: ImageMinimizerPlugin.loader,
+                options: {
+                    severityError: 'warning', // Ignore errors on corrupted images
+                    minimizerOptions: {
+                        plugins: ['gifsicle'],
+                    },
+                },
+            }],
+        }, {
+            test: /\.(glsl|frag|vert)/,
+            loader: 'raw-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.(glsl|frag|vert)/,
+            loader: 'glslify-loader',
+            exclude: /node_modules/
+
         }]
     }
 }
