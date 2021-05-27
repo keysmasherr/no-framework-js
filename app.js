458d249f1b8545555628c0b2234148e6e67c92c7
keysmasherr <keysmasherr@gmail.com>
2021-05-28 01:53:07 +0530

    added views and integrated pug

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/detail/:id', (req, res) => {
+  res.render('pages/detail');
+});
+
+app.get('/collections', (req, res) => {
+  res.render('pages/collections');
+});
+
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
