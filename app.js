84e75097ee4df1e359792ce91999b167700cea58
keysmasherr <keysmasherr@gmail.com>
2021-05-29 03:10:27 +0530

    add environment for prismic and integration in app integration to be refactored

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 const path = require('path');
+
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+
+// Initialize the prismic.io api
+const initApi = (req) => Prismic.getApi(
+  process.env.PRISMIC_ENDPOINT, {
+    req,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  },
+);
+
+const handleLinkResolver = (doc) => {
+
+  // Define the url depending on the document type
+  //   if (doc.type === 'page') {
+  //     return '/page/' + doc.uid;
+  //   } else if (doc.type === 'blog_post') {
+  //     return '/blog/' + doc.uid;
+  //   }
+
+  //   // Default to homepage
+  return '/';
+}
+
+// Middleware to inject prismic context
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+  };
+  // add PrismicDOM in locals to access them in templates.
+  res.locals.PrismicDOM = PrismicDOM;
+  next();
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -12,15 +48,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.render('pages/about');
+  initApi(req).then((api) => {
+    api.query([
+      Prismic.Predicates.any('document.type', ['metadata', 'about']),
+    ]).then(({ results }) => {
+      const [about, meta] = results;
+      console.log(meta);
+      res.render('pages/about', { about, meta });
+    });
+  });
 });
 
-app.get('/detail/:id', (req, res) => {
+app.get('/detail/:uid', (req, res) => {
   res.render('pages/detail');
 });
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections');
+app.get('/collection', (req, res) => {
+  res.render('pages/collection');
 });
 
 app.listen(port, () => {
